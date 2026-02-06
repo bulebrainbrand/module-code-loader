6480f7b44651b82b8ad3d3e5bbd5a8b33fd9d78e
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-02-06 10:18:06 +0900

    add license

--- loader.js
+++ loader.js
@@ -1,4 +1,18 @@
+/*!
+Copyright 2026 bulebrainbrand
 
+Licensed under the Apache License, Version 2.0 (the “License”);
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+https://github.com/bulebrainbrand/module-code-loader/main/LICENSE.txt
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an “AS IS” BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 // config //
 const SHOW_LOG = {
   error:true,
@@ -88,11 +102,21 @@ for(const name of USING_CALLBACK){
     return bloxdGame.dispatchEvent(name,...arg)
   }
 }
+/*
+Copyright 2026 GlitchHunterCoder
 
-// made by GlitchHunterCoder
-// apache2.0 license
-// https://github.com/GlitchHunterCoder/Bloxd-Async/blob/main/LICENSE
+Licensed under the Apache License, Version 2.0 (the “License”);
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+https://github.com/GlitchHunterCoder/Bloxd-Async/blob/main/LICENSE
 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an “AS IS” BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 globalThis.GeneratorFunction = function* () {}.constructor;
 globalThis.Generator = function* () {}().constructor;
 
